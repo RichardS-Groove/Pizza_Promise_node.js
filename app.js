4e36635b81c301bc92c0c4684100f968618f4ac3
Richard Campos <richardsjcb@gmail.com>
2022-08-11 16:32:55 -0300

    Se agrega estructura unificada

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const estatusPedido = () => {
-  const estatus = Math.random() < 0.8;
-  console.log(estatus);
-  return estatus;
+  return Math.random() < 0.8;
 };
 
 /* for (let i = 0; i < 10; i++) {
   console.log(estatusPedido());
 } */
+
+const miPedidoDePizza = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (estatusPedido()) {
+      resolve("Pedido exitoso! Su pizza esta en camino");
+    } else {
+      reject("Ocurrio un error. Por favor intente nuevamente");
+    }
+  }, 3000);
+});
+
+/* const manejarPedido = (mensajeDeConfirmacion) => {
+  console.log(mensajeDeConfirmacion);
+};
+
+const rechazarPedido = (mensajeDeError) => {
+  console.log(mensajeDeError);
+};
+
+miPedidoDePizza.then(manejarPedido, rechazarPedido); */
+
+miPedidoDePizza
+  .then((mensajeDeConfirmacion) => {
+    console.log(mensajeDeConfirmacion);
+  })
+  .then(null, (mensajeDeError) => {
+    console.log(mensajeDeError);
+  });
